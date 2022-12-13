6b348694bf563618960fcee63ee63009c390d98c
saberx45 <a.saberfree1@gmail.com>
2022-12-13 07:49:00 +0200

    testing dashboard accessibility + editing on the 404 page

--- dashboard/tests/test_dashboard.py
+++ dashboard/tests/test_dashboard.py
@@ -1,10 +1,23 @@
-from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.test import TestCase 
 from django.shortcuts import reverse 
-
+from users.models import Profile
 class TestingDashboard(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username="Test")
+        self.user.set_password("123456789")
+        self.user.save()
+        self.client.login(username='Test', password='123456789')
+        self.user.profile.committee = "PR"
+        self.user.profile.save()
+        
     def test_dashboard(self):
         url = reverse('Dashboard')
-        request = self.client.get(url)
-        self.assertEquals(request.status_code,302) #Testing login required
+        request = self.client.get(url)     
+        # testing dashboard page
+        self.assertEquals(request.status_code, 200)
 
-        self.assertTemplateUsed(request.templates,'dashboard/PR/dashboard.html')
+            
+        
+        
