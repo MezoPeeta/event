dbddff16ffa765b3df5866d819b7206976667531
saberx45 <a.saberfree1@gmail.com>
2022-12-30 14:57:51 +0200

    added speakers model + paginated speakers page + enhanced test_PRHR file

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from dashboard.forms import ContactForm
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Contact, Videos , Subscribe
+from .models import Contact, Videos , Subscribe, Speakers
 
 admin.site.site_header = "TEDx ManaratAlFarouk School"
 admin.site.site_title = "TEDx ManaratAlFarouk School"
@@ -25,4 +25,9 @@ class ContactModel(admin.ModelAdmin):
     list_display = ('name', 'subject', 'email')
 
 admin.site.register(Contact,ContactModel)
+
+class SpeakersModel(admin.ModelAdmin):
+    list_display = ('name', 'talk_name')
+
+admin.site.register(Speakers,SpeakersModel)
 
--- dashboard/tests/test_PRHR.py
+++ dashboard/tests/test_PRHR.py
@@ -2,61 +2,59 @@ from django.test import LiveServerTestCase
 from django.contrib.auth.models import User
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from django.urls import reverse
 
 class TestingPrHr(LiveServerTestCase):
-    def test_pr(self):
-        PATH = "C:/Users/abdo/Downloads/chromedriver_win32.exe"
-        self.browser = webdriver.Chrome(PATH)
-        dashboard = self.live_server_url + "/en//dashboard/" 
+    def setUp(self):
         #creating a user
         self.user = User.objects.create(username="Test")
         self.user.set_password("123456789")
-        self.user.profile.committee = "PR"
         self.user.save()
-        self.user.profile.save()
-
-        # entering user credentials
-        self.browser.get(dashboard)
-        username = self.browser.find_element(By.XPATH,'//*[@id="id_username"]')
-        username.send_keys("Test")
-        password = self.browser.find_element(By.XPATH,'//*[@id="id_password"]')
-        password.send_keys("123456789")
-        # logging in 
-        login_button = self.browser.find_element(By.XPATH,'/html/body/div[1]/div/div/form/div[2]/input')
-        login_button.click()
-        # navigating to profile
-        profile_button = self.browser.find_element(By.XPATH,'/html/body/nav/ul/li[7]/a')
-        profile_button.click()
-        # navigating to the dashbord
-        dashboard_button = self.browser.find_element(By.XPATH,'/html/body/div[1]/center/div/a[2]')
-        dashboard_button.click()
-        # adding video 
-        action_button = self.browser.find_element(By.XPATH,'/html/body/section/div/a[2]')
-        action_button.click()
-    def test_hr(self):
         PATH = "C:/Users/abdo/Downloads/chromedriver_win32.exe"
-        self.browser = webdriver.Chrome(PATH)
-        dashboard = self.live_server_url + "/en//dashboard/"
-        #creating a user
-        self.user = User.objects.create(username="Test2")
-        self.user.set_password("123456789")
-        self.user.profile.committee = "HR"
-        self.user.save()
+        #loggin the user in
+        self.client.login(username="Test", password='123456789') #Native django test client
+        cookie = self.client.cookies['sessionid']
+        self.browser = webdriver.Chrome(PATH)         
+        self.browser.get(self.live_server_url + '/us/')  #selenium will set cookie domain based on current page domain
+        self.browser.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
+        self.browser.refresh() #need to update page for logged in user
+        self.browser.get(self.live_server_url + '/us/')      
+    def test_pr(self):
+        #saving the test profile as a PR member
+        dashboard_page = reverse('Dashboard')
+        dashboard = self.live_server_url + dashboard_page
+        self.user.profile.committee = "PR"
         self.user.profile.save()
-
-        # entering user credentials
+        #accessing the dashboard
         self.browser.get(dashboard)
-        username = self.browser.find_element(By.XPATH,'//*[@id="id_username"]')
-        username.send_keys("Test2")
-        password = self.browser.find_element(By.XPATH,'//*[@id="id_password"]')
-        password.send_keys("123456789")
-        # logging in 
-        login_button = self.browser.find_element(By.XPATH,'/html/body/div[1]/div/div/form/div[2]/input')
-        login_button.click()
-        # navigating to profile
-        profile_button = self.browser.find_element(By.XPATH,'/html/body/nav/ul/li[7]/a')
-        profile_button.click()
-        # navigating to the dashbord
-        dashboard_button = self.browser.find_element(By.XPATH,'/html/body/div[1]/center/div/a[2]')
-        dashboard_button.click()
+        request = self.client.get(dashboard_page)
+        #checking the url + status code + comittee
+        current_url = self.browser.current_url
+        self.assertEqual(current_url, dashboard)
+        self.assertEqual(request.status_code,200)
+        self.assertFalse(self.user.profile.committee != "PR")
+    def test_hr(self):
+        #saving the test profile as a HR member
+        dashboard_page = reverse('Inbox')
+        dashboard = self.live_server_url + dashboard_page
+        contact_page = reverse('Contact')
+        self.user.profile.committee = "HR"
+        self.user.profile.save()
+        #accessing the dashboard
+        self.browser.get(dashboard)
+        self.browser.get(self.live_server_url + contact_page)
+        #  creating a contact report
+        name = self.browser.find_element(By.XPATH, '/html/body/section/div/div[2]/div[1]/form/div[1]/input')
+        name.send_keys("Tester")
+        email = self.browser.find_element(By.XPATH, '/html/body/section/div/div[2]/div[1]/form/div[2]/input')
+        email.send_keys("Tester@gmail.com")
+        subject = self.browser.find_element(By.XPATH, '/html/body/section/div/div[2]/div[1]/form/div[3]/input')
+        subject.send_keys("Testing")
+        message = self.browser.find_element(By.XPATH, '/html/body/section/div/div[2]/div[1]/form/div[4]/textarea')
+        message.send_keys("Test")
+        submit_btn = self.browser.find_element(By.XPATH, '/html/body/section/div/div[2]/div[1]/form/div[5]/input')
+        submit_btn.click()
+        #checking the contact in the inbox page
+        self.browser.get(dashboard)
+      
         
