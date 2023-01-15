c123ba0e5ab21b3935548fa71755dc35400d6307
Mazen Omar <wildlifemain1@gmail.com>
2023-01-15 18:23:00 +0200

    Selenium Config

--- base/admin.py
+++ base/admin.py
@@ -1,32 +1,36 @@
 from dashboard.forms import ContactForm
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import Contact, Videos , Subscribe, Speakers
+from .models import Contact, Videos, Subscribe, Speakers
 
 admin.site.site_header = "TEDx ManaratAlFarouk School"
 admin.site.site_title = "TEDx ManaratAlFarouk School"
 
 
 class VideosModel(admin.ModelAdmin):
-    fields = ('urlID' , 'name')
-    list_display = ('name' , 'date_posted', 'user')
+    fields = ("urlID", "name")
+    list_display = ("name", "date_posted", "user")
+
 
 admin.site.register(Videos, VideosModel)
 
+
 class SubscribeModel(admin.ModelAdmin):
-    list_display = ('name' , 'email','subscribed','date_subscribed')
+    list_display = ("name", "email", "subscribed", "date_subscribed")
 
 
 admin.site.register(Subscribe, SubscribeModel)
 
 
-
 class ContactModel(admin.ModelAdmin):
-    list_display = ('name', 'subject', 'email')
+    list_display = ("name", "subject", "email")
 
-admin.site.register(Contact,ContactModel)
+
+admin.site.register(Contact, ContactModel)
+
 
 class SpeakersModel(admin.ModelAdmin):
-    list_display = ('name', 'talk_name')
-    
-admin.site.register(Speakers,SpeakersModel)
+    list_display = ("name", "talk_name")
+
+
+admin.site.register(Speakers, SpeakersModel)
