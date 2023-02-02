aa29144a294350b9f264efb75a4a8ad00013c54e
Mazen Omar <wildlifemain1@gmail.com>
2023-02-02 23:27:56 +0200

    Design Dashboard + Fixed ER Bugs

--- utils/selenium_test.py
+++ utils/selenium_test.py
@@ -7,7 +7,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver import ActionChains
-from django.tests import LiveServerTestCase
+from django.test import LiveServerTestCase
 
 
 class Browser(Enum):
