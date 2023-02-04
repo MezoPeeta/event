5025771eb43e6c11a78e48a3e21e99544be22843
Mazen Omar <wildlifemain1@gmail.com>
2023-02-05 01:19:31 +0200

    Adding auto add project

--- payment/models.py
+++ payment/models.py
@@ -39,7 +39,7 @@ class TicketRecieved(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     email = models.EmailField()
-    code = models.OneToOneField(QrCode , on_delete=models.SET_NULL, null=True)
+    code = models.OneToOneField(QrCode , on_delete=models.CASCADE, blank=True , null=True)
     def __str__(self):
         return f'{self.name}'
 
