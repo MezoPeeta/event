af15abe41fba447625152bf0e3e9defb6a6ded5e
saberx45 <a.saberfree1@gmail.com>
2023-02-04 09:07:03 +0200

    fixed price color and added the name field in new_products.html

---
+++ products/tests/test_functionsreturns.py
@@ -0,0 +1,15 @@
+from django.test import TestCase 
+from products.models import Products, Customer
+
+class TestingRetruns(TestCase):
+    def test_products(self):
+        self.product = Products.objects.create(price=200)
+        self.assertEquals(self.product.name, "Product Name")
+
+
+    def test_customers(self):
+        self.customer1 = Customer.objects.create(name='testcustomer', device='testdevice')
+        self.assertEquals(self.customer1.name, "testcustomer")
+        self.customer2 = Customer.objects.create(device='testdevice2')
+        self.assertEquals(str(self.customer2), "testdevice2")
+        
