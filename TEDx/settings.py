68806438ff202ca4c74139c133d2dd7e8255cc52
Mazen Omar <wildlifemain1@gmail.com>
2023-02-07 20:53:15 +0200

    Test production

--- TEDx/settings.py
+++ TEDx/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = "z#j+0d9jx0yme*if+&7htan5@u-2o=ln+x^x%0uv=ypg@jb8jz"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://event-production-ec41.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
