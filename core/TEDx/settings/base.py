0086311969b3e97b362ed4a3d68bf3db374f2328
Mostafa <fastsafa.2006@gmail.com>
2023-05-06 00:58:17 +0300

    fixed product search bar test

--- core/TEDx/settings/base.py
+++ core/TEDx/settings/base.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import main
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-load_dotenv()
+main.load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
